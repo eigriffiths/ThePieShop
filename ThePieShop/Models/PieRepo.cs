23a9cec41278c114b35f7a71fc6c122299757e95
emyr <emyr.griffiths@gmail.com>
2018-08-06 21:00:24 +0100

    Part5

---
+++ ThePieShop/Models/PieRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePieShop.Models
+{
+    public class PieRepo : IPieRepo
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepo(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDbContext.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
--- ThePieShop/Startup.cs
+++ ThePieShop/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ThePieShop.Models;
 
@@ -12,11 +14,21 @@ namespace ThePieShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IPieRepo, MockPieRepo>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IPieRepo, PieRepo>();
+
             services.AddMvc();
         }
 
